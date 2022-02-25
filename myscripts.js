92d35d57820e79d3cdba25b2848661d0ec61931a
MemoByz <99743124+MemoByz@users.noreply.github.com>
2022-02-25 17:05:40 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -31,57 +31,53 @@ burgerMenu();
 //List Products
 let productsData = [
 
-      { Produkt: "Tomaten", Preis: 2.50, Anzahl: 23},
-      { Produkt: "Kartoffel", Preis: 3.50, Anzahl: 12},
-      { Produkt: "Paprika", Preis: 1.50, Anzahl: 22},
-      { Produkt: "Gurken", Preis: 1.15, Anzahl: 37},
-      { Produkt: "Bananen", Preis: 1.20, Anzahl: 25},
-      { Produkt: "Kirschen", Preis: 1.99, Anzahl: 11},
-      { Produkt: "Trauben", Preis: 1.50, Anzahl: 26},
-      { Produkt: "Auberginen", Preis: 3.50, Anzahl: 18},
-      { Produkt: "Petersillien", Preis: 2.15, Anzahl: 15}
+      { Produkt: "Tomaten", Preis: 2.50, Anzahl: 23, inCart: 0},
+      { Produkt: "Kartoffel", Preis: 3.50, Anzahl: 12, inCart: 0},
+      { Produkt: "Paprika", Preis: 1.50, Anzahl: 22, inCart: 0},
+      { Produkt: "Gurken", Preis: 1.15, Anzahl: 37, inCart: 0},
+      { Produkt: "Bananen", Preis: 1.20, Anzahl: 25, inCart: 0},
+      { Produkt: "Kirschen", Preis: 1.99, Anzahl: 11, inCart: 0},
+      { Produkt: "Trauben", Preis: 1.50, Anzahl: 26, inCart: 0},
+      { Produkt: "Auberginen", Preis: 3.50, Anzahl: 18, inCart: 0},
+      { Produkt: "Petersillien", Preis: 2.15, Anzahl: 15, inCart: 0}
    ];
 
-
+   
 
 function loadTableData(productsData){
    const tableBody = document.getElementById('products__data_id');
    let dataHtml = '';
+   let counter = 0;
 
 
-   for(let data of productsData){
-      dataHtml += `<tr><td>${data.Produkt}</td><td>${data.Preis}</td><td>${data.Anzahl}</td><td><a id="add-cart-id" class="add-cart" href="#">in den Warenkorb</a></td></tr>`;
+   for(let i of productsData){
+      dataHtml += `<tr><td>${i.Produkt}</td><td>${i.Preis} €</td><td>${i.Anzahl}</td><td><a onclick="cartNumbers(productsData[${counter}]); totalCost(productsData[${counter}]);" class="add-cart" href="#">in den Warenkorb</a></td></tr>`;
+      counter++;
    }
+
 
    tableBody.innerHTML = dataHtml;
 }
 
 
-
-
-
-
-
 //Shopping Cart
-let carts = document.querySelectorAll('.add-cart');
+/*
+let cartButtons = document.querySelectorAll('#products__data_id .add-cart');
+console.log(cartButtons);
 
-
-for (let i=0; i < carts.length; i++){
-   carts[i].addEventListener('click', () => {
+for (let i=0; i < cartButtons.length; i++){
+   console.log("this is a button")
+   cartButtons[i].addEventListener('click', () => {
       cartNumbers();
    })
-}
+} */
 
 
-
-
-
-
-function cartNumbers(){
+function cartNumbers(product){
    let productNumbers =sessionStorage.getItem('cartNumbers');
 
    productNumbers = parseInt(productNumbers);
-
+   
    if(productNumbers){
       sessionStorage.setItem('cartNumbers', productNumbers + 1);
       document.querySelector('.navbar__navlinks span').textContent = productNumbers + 1;
@@ -89,6 +85,33 @@ function cartNumbers(){
       sessionStorage.setItem('cartNumbers', 1);
       document.querySelector('.navbar__navlinks span').textContent = 1;
    }
+
+   setItems(product);
+}
+
+
+function setItems(product){
+   let cartItems = sessionStorage.getItem('productsInCart');
+   cartItems = JSON.parse(cartItems);
+   
+   if(cartItems != null){
+      
+      if(cartItems[product.Produkt] == undefined){
+         cartItems = {
+            ...cartItems, 
+            [product.Produkt]: product
+         }
+      }
+      cartItems[product.Produkt].inCart += 1;
+   }else{
+      product.inCart = 1;
+      cartItems = {
+         [product.Produkt]: product
+      }
+   }
+   
+
+   sessionStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
 
@@ -105,6 +128,48 @@ function onLoadcartNumbers(){
 
 onLoadcartNumbers();
 
+
+
+//costs table
+function totalCost(product){
+  let cartCost = sessionStorage.getItem('totalCost');
+
+  product.Anzahl -= 1;
+  loadTableData(productsData);
+
+  if(cartCost != null){
+     cartCost = parseFloat(cartCost);
+     sessionStorage.setItem("totalCost", cartCost + product.Preis);
+  }else{
+   sessionStorage.setItem("totalCost", product.Preis);
+  }
+}
+
+function displayCart(){
+   let cartItems = sessionStorage.getItem("productsInCart");
+   cartItems = JSON.parse(cartItems);
+   let productContainer = document.querySelector(".shoppingcart__costtable-list");
+
+   console.log(cartItems);
+   if(cartItems && productContainer){
+      productContainer.innerHTML = '';
+      Object.values(cartItems).map(item => {
+         productContainer.innerHTML += `
+            <div class="product"><ion-icon name="close-circle-outline"></ion-icon><span>${item.Produkt}</span></div>
+            <div class="price">${item.Preis}</div>
+            <div class="quantity">
+               <ion-icon name="arrow-back-circle-outline"></ion-icon>
+               <span>${item.inCart}</span>
+               <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+            </div>
+            `
+      })
+   }
+
+}
+
+
+displayCart();
 
 
 
