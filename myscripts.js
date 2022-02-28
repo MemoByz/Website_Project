1eaa103b1bfb83de7ce15bf1cf63557903a2b594
MemoByz <99743124+MemoByz@users.noreply.github.com>
2022-02-28 16:39:12 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -31,15 +31,15 @@ burgerMenu();
 //List Products
 let productsData = [
 
-      { Produkt: "Tomaten", Preis: 2.50, Anzahl: 23, inCart: 0},
-      { Produkt: "Kartoffel", Preis: 3.50, Anzahl: 12, inCart: 0},
-      { Produkt: "Paprika", Preis: 1.50, Anzahl: 22, inCart: 0},
-      { Produkt: "Gurken", Preis: 1.15, Anzahl: 37, inCart: 0},
-      { Produkt: "Bananen", Preis: 1.20, Anzahl: 25, inCart: 0},
+      { Produkt: "Tomaten", Preis: 2.99, Anzahl: 23, inCart: 0},
+      { Produkt: "Kartoffel", Preis: 3.89, Anzahl: 12, inCart: 0},
+      { Produkt: "Paprika", Preis: 1.89, Anzahl: 22, inCart: 0},
+      { Produkt: "Gurken", Preis: 0.88, Anzahl: 37, inCart: 0},
+      { Produkt: "Bananen", Preis: 0.99, Anzahl: 25, inCart: 0},
       { Produkt: "Kirschen", Preis: 1.99, Anzahl: 11, inCart: 0},
-      { Produkt: "Trauben", Preis: 1.50, Anzahl: 26, inCart: 0},
-      { Produkt: "Auberginen", Preis: 3.50, Anzahl: 18, inCart: 0},
-      { Produkt: "Petersillien", Preis: 2.15, Anzahl: 15, inCart: 0}
+      { Produkt: "Trauben", Preis: 1.49, Anzahl: 26, inCart: 0},
+      { Produkt: "Auberginen", Preis: 2.49, Anzahl: 18, inCart: 0},
+      { Produkt: "Petersillien", Preis: 1.15, Anzahl: 15, inCart: 0}
    ];
 
    
@@ -51,7 +51,8 @@ function loadTableData(productsData){
 
 
    for(let i of productsData){
-      dataHtml += `<tr><td>${i.Produkt}</td><td>${i.Preis} €</td><td>${i.Anzahl}</td><td><a onclick="cartNumbers(productsData[${counter}]); totalCost(productsData[${counter}]);" class="add-cart" href="#">in den Warenkorb</a></td></tr>`;
+      dataHtml += `<tr><td>${i.Produkt}</td><td>${i.Preis} €</td><td>${i.Anzahl}</td><td><a onclick="cartNumbers(productsData[${counter}]); 
+      totalCost(productsData[${counter}]);" class="add-cart" href="#">in den Warenkorb</a></td></tr>`;
       counter++;
    }
 
@@ -60,17 +61,26 @@ function loadTableData(productsData){
 }
 
 
-//Shopping Cart
-/*
-let cartButtons = document.querySelectorAll('#products__data_id .add-cart');
-console.log(cartButtons);
 
-for (let i=0; i < cartButtons.length; i++){
-   console.log("this is a button")
-   cartButtons[i].addEventListener('click', () => {
-      cartNumbers();
-   })
-} */
+
+//Sort function
+let isAscending = false;
+
+function sortColumn(columnName){
+
+   if(isAscending==true){
+      isAscending = !isAscending;
+      productsData = productsData.sort((a,b) => a[columnName] > b[columnName] ? 1 : -1)
+   }else{
+      isAscending = true;
+      productsData = productsData.sort((a,b) => a[columnName] < b[columnName] ? 1 : -1)
+   }
+
+   loadTableData(productsData);
+}
+
+
+
 
 
 function cartNumbers(product){
@@ -88,6 +98,9 @@ function cartNumbers(product){
 
    setItems(product);
 }
+
+
+
 
 
 function setItems(product){
@@ -117,6 +130,7 @@ function setItems(product){
 
 
 
+
 function onLoadcartNumbers(){
    let productNumbers = sessionStorage.getItem('cartNumbers');
 
@@ -130,7 +144,8 @@ onLoadcartNumbers();
 
 
 
-//costs table
+
+
 function totalCost(product){
   let cartCost = sessionStorage.getItem('totalCost');
 
@@ -143,27 +158,41 @@ function totalCost(product){
   }else{
    sessionStorage.setItem("totalCost", product.Preis);
   }
+
+  
+
 }
+
+
+
+
 
 function displayCart(){
    let cartItems = sessionStorage.getItem("productsInCart");
    cartItems = JSON.parse(cartItems);
    let productContainer = document.querySelector(".shoppingcart__costtable-list");
+   let cartCost = sessionStorage.getItem('totalCost');
+   cartCost = Math.round(cartCost*100)/100;
+
 
    console.log(cartItems);
    if(cartItems && productContainer){
       productContainer.innerHTML = '';
       Object.values(cartItems).map(item => {
          productContainer.innerHTML += `
-            <div class="product"><ion-icon name="close-circle-outline"></ion-icon><span>${item.Produkt}</span></div>
-            <div class="price">${item.Preis}</div>
-            <div class="quantity">
-               <ion-icon name="arrow-back-circle-outline"></ion-icon>
-               <span>${item.inCart}</span>
-               <ion-icon name="arrow-forward-circle-outline"></ion-icon>
-            </div>
-            `
-      })
+            <div class="product">${item.Produkt}</div>
+            <div class="product-price">${item.Preis} €</div>
+            <div class="product-quantity">${item.inCart}</div>
+            <div class="product-total"> ${item.inCart * item.Preis} €</div>
+            `;
+      });
+
+
+      productContainer.innerHTML += `
+         <div class="cartTotalContainer">
+            <h4 class="cartTotalTitel">Gesamtpreis: </h4>
+            <h4 class="cartTotal">${cartCost} €</h4>
+         </div>`
    }
 
 }
@@ -177,22 +206,6 @@ displayCart();
 
 
 
-//Sort function
-let isAscending = false;
-
-
-function sortColumn(columnName){
-
-   if(isAscending==true){
-      isAscending = !isAscending;
-      productsData = productsData.sort((a,b) => a[columnName] > b[columnName] ? 1 : -1)
-   }else{
-      isAscending = true;
-      productsData = productsData.sort((a,b) => a[columnName] < b[columnName] ? 1 : -1)
-   }
-
-   loadTableData(productsData);
-}
 
 
 
